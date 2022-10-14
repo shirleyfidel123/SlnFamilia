dfdbf61c225f99b6095e98489c2805d37ef8c16e
Maximo Bravo <bravomaximo17@gmail.com>
2022-10-13 21:27:32 -0300

    v1.0

---
+++ MiProyectoUno/Form1.Designer.cs
@@ -0,0 +1,102 @@
+﻿
+namespace MiProyectoUno
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.Completar = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.dgvFamilia = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvFamilia)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // Completar
+            // 
+            this.Completar.Location = new System.Drawing.Point(571, 96);
+            this.Completar.Name = "Completar";
+            this.Completar.Size = new System.Drawing.Size(75, 23);
+            this.Completar.TabIndex = 0;
+            this.Completar.Text = "Completar";
+            this.Completar.UseVisualStyleBackColor = true;
+            this.Completar.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(571, 326);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "button2";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(571, 206);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 2;
+            this.button3.Text = "button3";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // dgvFamilia
+            // 
+            this.dgvFamilia.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvFamilia.Location = new System.Drawing.Point(12, 12);
+            this.dgvFamilia.Name = "dgvFamilia";
+            this.dgvFamilia.Size = new System.Drawing.Size(386, 426);
+            this.dgvFamilia.TabIndex = 3;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.dgvFamilia);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.Completar);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dgvFamilia)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button Completar;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.DataGridView dgvFamilia;
+    }
+}
+
---
+++ MiProyectoUno/Modelo.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;
+using MySql.Data.MySqlClient;
+
+namespace Modelo
+{
+    class Familia
+    {
+        public int idfamilia { get; set; }
+        public string nombre { get; set; }
+        public string apellido { get; set; }
+        public string parentesco { get; set; }
+        public string ocupacion { get; set; }
+
+        public DataTable LlenarFamilia()
+        {
+            MySqlConnection conexion = new MySqlConnection("server=10.120.3.200;userid=alumn517;password=Alumno2022;database=repo_517");
+            MySqlCommand instruccion = new MySqlCommand();
+            instruccion.Connection = conexion;
+
+            conexion.Open();
+            MySqlDataAdapter Adaptador = new MySqlDataAdapter();
+            instruccion.CommandText = "select * from familia";
+            Adaptador.SelectCommand = instruccion;
+            DataSet DBAF = new DataSet();
+            Adaptador.Fill(DBAF);
+            DataTable Tabla = new DataTable();
+            Tabla = DBAF.Tables[0];
+            conexion.Close();
+            return Tabla;
+        }
+    }
+}
